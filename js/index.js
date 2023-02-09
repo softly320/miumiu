e5d73d9a231885310588eaba2da1cc5e5fab66a8
softly320 <122245757+softly320@users.noreply.github.com>
2023-02-09 09:46:21 +0900

    0209

--- js/index.js
+++ js/index.js
@@ -60,140 +60,140 @@ $(document).ready(function () {
     let club_list = [
         design = {
             id : 1,
-            image : "/images/campa1.png",
+            image : "images/campa1.png",
             title: "프라이빗 위시",
             desc : "2022 홀리데이 컬렉션",
             iso: " campa"
         },
         {
             id : 2,
-            image : "/images/fashi1.png",
+            image : "images/fashi1.png",
             title: "SPRING SUMMER 2023",
             desc : "In Translation",
             iso: " fashi"
         },
         {
             id : 3,
-            image : "/images/woman1.png",
+            image : "images/woman1.png",
             title: "#21 샹그릴라",
             desc : "이자벨 산도발(Isabel Sandoval)",
             iso: " womans"
         },
         {
             id : 4,
-            image : "/images/special1.png",
+            image : "images/special1.png",
             title: "미우미우 테니스 클럽",
             desc : "생 트로페즈",
             iso: " special"
         },
         {
             id : 5,
-            image : "/images/perfume1.png",
+            image : "images/perfume1.png",
             title: "미우미우 트위스트 오 드 매그놀리아",
             desc : "후각을 자극하는 여정",
             iso: " perfume"
         },
         {
             id : 6,
-            image : "/images/campa2.png",
+            image : "images/campa2.png",
             title: "미우 완더",
             desc : "배회, 탐험, 호기심 충족 발견",
             iso: " campa"
         },
         {
             id : 7,
-            image : "/images/campa3.png",
+            image : "images/campa3.png",
             title: "CHARACTER STUDY",
             desc : "미우미우 2022 가을/겨울 캠페인",
             iso: " campa"
         },
         {
             id : 7,
-            image : "/images/campa4.png",
+            image : "images/campa4.png",
             title: "BASIC INSTINCTS",
             desc : "미우미우 2022 봄/여름 캠페인",
             iso: " campa"
         },
         {
             id : 8,
-            image : "/images/fashi2.png",
+            image : "images/fashi2.png",
             title: "2022 가을 겨울",
             desc : "Nathalie Djurberg and Hans Berg",
             iso: " fashi"
         },
         {
             id : 9,
-            image : "/images/fashi3.png",
+            image : "images/fashi3.png",
             title: "2022 봄/여름",
             desc : "Basic Instincts",
             iso: " fashi"
         },
         {
             id : 10,
-            image : "/images/fashi4.png",
+            image : "images/fashi4.png",
             title: "2021 가을 겨울",
             desc : "브레이브 하트",
             iso: " fashi"
         },
         {
             id : 11,
-            image : "/images/woman2.png",
+            image : "images/woman2.png",
             title: "#22 I AND THE STUPID BOY",
             desc : "카우테르 벤 하니아(Kaouther Ben Hania)",
             iso: " womans"
         },
         {
             id : 12,
-            image : "/images/woman3.png",
+            image : "images/woman3.png",
             title: "#23 HOUSE COMES WITH A BIRD",
             desc : "자니크자 브라보(Janicza Bravo)",
             iso: " womans"
         },
         {
             id : 13,
-            image : "/images/woman4.png",
+            image : "images/woman4.png",
             title: "#24 CARTA A MI MADRE PARA MI HIJO",
             desc : "카를라 시몬(Carla Simón)",
             iso: " womans"
         },
         {
             id : 14,
-            image : "/images/special2.png",
+            image : "images/special2.png",
             title: "UPCYCLED BY MIU MIU",
             desc : "Re-worked,re-fashioned. by Miu Miu",
             iso: " special"
         },
         {
             id : 15,
-            image : "/images/special3.png",
+            image : "images/special3.png",
             title: "A REMEDY",
             desc : "나탈리 뒤버그(Nathalie Djurberg)",
             iso: " special"
         },
         {
             id : 16,
-            image : "/images/special4.png",
+            image : "images/special4.png",
             title: "샤인 어 라이트",
             desc : "짜릿한 새해를 알리는 미우미우",
             iso: " special"
         },
         {
             id : 17,
-            image : "/images/perfume2.png",
+            image : "images/perfume2.png",
             title: "Twist Eau de Parfum",
             desc : "미우미우 트위스트(Twist)",
             iso: " perfume"
         },
         {
             id : 18,
-            image : "/images/perfume3.png",
+            image : "images/perfume3.png",
             title: "Miu Miu L’Eau Rosée",
             desc : "말썽꾸러기가 되어 보세요!",
             iso: " perfume"
         },
         {
             id : 19,
-            image : "/images/perfume4.png",
+            image : "images/perfume4.png",
             title: "Miu Miu Fleur d'Argent",
             desc : "눈부신 황홀함에 빠져 보세요.",
             iso: " perfume"
@@ -260,10 +260,10 @@ $(document).ready(function () {
         ]
     
         let design_img=[
-            "/images/bag1Cp.jpg",
-            "/images/bag1Cg.jpg",
-            "/images/bag1Cb.jpg",
-            "/images/bag1Cw.jpg"
+            "images/bag1Cp.jpg",
+            "images/bag1Cg.jpg",
+            "images/bag1Cb.jpg",
+            "images/bag1Cw.jpg"
         ]
     
         $(".bag .content-bag ul li p, .hot .content2 .buy-btn").click(function(e){
